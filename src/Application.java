5a3216e9482823c99e309f94b48873baf1abff4a
Daniel Schulz <daniel.schulz@workmotion.com>
2021-12-25 11:12:04 +0100

    Lösung 22 hinein

--- src/Application.java
+++ src/Application.java
@@ -12,8 +12,20 @@ public class Application {
             new String[]{leer, leer, "6", leer, leer, "2", leer, leer, leer},
     };
 
+    private static String[][] lösung22 = new String[][]{
+            new String[]{"2", "5", "7", "9", "3", "6", "1", "4", "8"},
+            new String[]{"6", "3", "8", "1", "4", "7", "2", "5", "9"},
+            new String[]{"4", "1", "9", "2", "8", "5", "3", "7", "6"},
+            new String[]{"7", "6", "5", "4", "2", "9", "8", "3", "1"},
+            new String[]{"3", "8", "4", "6", "7", "1", "5", "9", "2"},
+            new String[]{"9", "2", "1", "8", "5", "3", "7", "6", "4"},
+            new String[]{"5", "4", "2", "3", "6", "8", "9", "1", "7"},
+            new String[]{"8", "9", "3", "7", "1", "4", "6", "2", "5"},
+            new String[]{"1", "7", "6", "5", "9", "2", "4", "8", "3"},
+    };
+
     public static void main(String[] args) {
-        sudokuDrucken(aufgabe31);
+        sudokuDrucken(lösung22);
     }
 
     private static void sudokuDrucken(String[][] sudoku) {
