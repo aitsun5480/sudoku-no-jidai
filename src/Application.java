ac7744f78fe504df778989dc780c48d8263ae5c3
Daniel Schulz <daniel.schulz@workmotion.com>
2021-12-25 11:06:17 +0100

    Sudoku Aufgabe extrahieren

--- src/Application.java
+++ src/Application.java
@@ -1,23 +1,22 @@
 public class Application {
+    private static String leer = "_";
+    private static String[][] aufgabe31 = new String[][]{
+            new String[]{leer, leer, leer, leer, leer, leer, leer, "3", leer},
+            new String[]{leer, "4", "9", "5", leer, leer, "1", leer, leer},
+            new String[]{"3", leer, leer, "4", leer, leer, leer, leer, leer},
+            new String[]{"2", leer, leer, leer, leer, leer, leer, "9", leer},
+            new String[]{"6", leer, leer, leer, "8", leer, leer, leer, "5"},
+            new String[]{leer, leer, "5", "9", "6", leer, leer, leer, "2"},
+            new String[]{leer, leer, leer, leer, leer, "1", "8", leer, leer},
+            new String[]{leer, leer, leer, leer, leer, leer, "9", "5", leer},
+            new String[]{leer, leer, "6", leer, leer, "2", leer, leer, leer},
+    };
+
     public static void main(String[] args) {
-        String leer = "_";
-
-        String[][] sudoku = new String[][]{
-                new String[]{leer, leer, leer, leer, leer, leer, leer, "3", leer},
-                new String[]{leer, "4", "9", "5", leer, leer, "1", leer, leer},
-                new String[]{"3", leer, leer, "4", leer, leer, leer, leer, leer},
-                new String[]{"2", leer, leer, leer, leer, leer, leer, "9", leer},
-                new String[]{"6", leer, leer, leer, "8", leer, leer, leer, "5"},
-                new String[]{leer, leer, "5", "9", "6", leer, leer, leer, "2"},
-                new String[]{leer, leer, leer, leer, leer, "1", "8", leer, leer},
-                new String[]{leer, leer, leer, leer, leer, leer, "9", "5", leer},
-                new String[]{leer, leer, "6", leer, leer, "2", leer, leer, leer},
-        };
-
-        sudokuDrucken(sudoku);
+        sudokuDrucken(aufgabe31);
     }
 
-    private static void sudokuDrucken(String[][] sudoku){
+    private static void sudokuDrucken(String[][] sudoku) {
         for (int zähler = 0; zähler < 9; zähler = zähler + 1) {
             sudokuReiheDrucken(sudoku[zähler]);
 
@@ -43,11 +42,11 @@ public class Application {
         neueReiheDrucken();
     }
 
-    private static void drucken(String text){
+    private static void drucken(String text) {
         System.out.print(text);
     }
 
-    private static void neueReiheDrucken(){
+    private static void neueReiheDrucken() {
         System.out.println();
     }
 }
